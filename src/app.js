df72747766d790c4100d547875dbbbb16f59a76c
Irakli Zhuzhunashvili <irakli.zhuzhunashvili1@gmail.com>
2015-04-30 17:52:40 -0600

    Added the ability to write to a text field simultaneously (based off of a tutorial, needs to be changed shince it's not using persistent storage)

---
+++ src/app.js
@@ -0,0 +1,75 @@
+// var express = require('express');
+// var _ = require('lodash');
+// var app = express();
+
+// app.use(express.static(__dirname + '/public'));
+
+
+
+// app.get('/', function(req, res) {
+//     res.render('index.html');
+// });
+
+// app.set('port', (process.env.PORT || 3000));
+
+// var server = app.listen(app.get('port'), function() {
+//     var host = server.address().address;
+//     var port = server.address().port;
+//     console.log('App listening at http://%s:%s', host, port);
+// });
+
+var Duplex = require('stream').Duplex;
+var connect = require('connect');
+var http = require('http');
+var serveStatic = require('serve-static');
+var browserChannel = require('browserchannel').server;
+var livedb = require('livedb');
+
+// This is sharejs server with no persistence, need to change
+var backend = livedb.client(livedb.memory());
+backend.submit('textareas', 'textarea1', { create: {type: 'text', data: ''}},
+    function (err, version, transformedByOps, snapshot) {
+        console.log('Created document. Err: ' + err + ". Versoin: " + version);
+    }
+);
+
+var sharejs = require('share');
+var share = sharejs.server.createClient({ backend: backend });
+
+var app = connect();
+
+app.use(serveStatic(__dirname + "/../public"));
+app.use(serveStatic(sharejs.scriptsDir));
+app.use(browserChannel({ cors: '*' }, function (client) {
+    var stream = new Duplex({ objectMode: true});
+
+    stream._read = function () {};
+    stream._write = function (chunk, encoding, callback) {
+        if (client.state !== 'closed') {
+            client.send(chunk);
+        }
+        callback();
+    };
+
+    client.on('message', function (data) {
+        stream.push(data);
+    });
+
+    client.on('close', function (reason) {
+        stream.push(null);
+        stream.emit('close');
+    });
+
+    stream.on('end', function () {
+        client.close();
+    });
+
+    return share.listen(stream);
+}));
+
+var server = http.createServer(app);
+
+var port = 3000
+server.listen(port, function() {
+    console.log('Listening on ' + port);
+});
